0679ef84637f175534f291b5d9dbca1d38fa838f
shubham shrivastav <you@example.com>
2022-12-20 23:48:43 +0530

    first commit for greet App

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+const Input = ({labelValue, txtInput}) => {
+    const placeHolder = `Type ${labelValue} Name Here`;
+    const takeInput = (event)=>{
+        const txtBoxValue = event.target.value;
+        //console.log('TextBox Value ', txtBoxValue);
+        txtInput(txtBoxValue, labelValue); // child call parent fn and pass textbox value to the parent
+    }
+  return (
+    <div>
+        <label>{labelValue} Name</label>
+        <input onChange={takeInput} className='form-control' type='textbox' placeholder={placeHolder}/>
+    </div>
+  )
+}
+
+export default Input
---
+++ src/pages/Greet.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import Button from "../components/Button"
+import Input from "../components/Input"
+import { Message } from "../components/Message"
+
+export const Greet = ()=>{
+    const [msgName, setMsgName] = useState('')
+    const names = {};
+    let fullName = "";
+    const recInputValue = (val, label)=>{
+        names[label.toLowerCase()] = val;
+        console.log('Names ', names);
+        //firstName = val;
+        //console.log('First Name is ', firstName);
+    }
+
+    const initCap = (currentName)=>
+        currentName.charAt(0).toUpperCase() + currentName.substring(1).toLowerCase()+" ";
+    
+
+    const greet=()=>{
+        for(let key in names){
+            const val = names[key];
+            fullName += initCap(val);
+        }
+        setMsgName('Welcome ' +fullName.trim());
+    }
+
+    return (<div>
+            <Message myclass="alert-info" msg="Greet App "/>
+            <Input txtInput = {recInputValue} labelValue="First"/>
+            <Input txtInput = {recInputValue} labelValue="Last"/>
+            <Button operation = {greet} labelValue="Greet" myClass="primary"/> &nbsp;
+            <Button labelValue="Clear All" myClass="secondary"/>
+            <Message myclass="alert-success" msg = {msgName}/>
+    </div>)
+}
