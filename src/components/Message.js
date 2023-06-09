7f18b8aba24fc102765049a5c7178b0dac8d0cbd
shubham shrivastav <shubhamshrivastav16@gmail.com>
2023-06-09 16:04:27 +0530

    adding axios and redux toolit library

--- src/components/Message.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const Message = ({msg, myclass}) => {
-    const h1Class = `${myclass} text-center`;
-  return (
-    <h1 className = {h1Class}>{msg}</h1>
-  )
-}
